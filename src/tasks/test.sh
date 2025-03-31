aff07051bdb805fed5d950c40039cebbfbe96936
Mykhailo Seheda <147707994+mseheda@users.noreply.github.com>
2025-03-31 20:27:20 +0300

    Update test.sh

--- src/tasks/test.sh
+++ src/tasks/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
- aarch64-linux-gnu-as -o task-1.o task-1.S
+ aarch64-linux-gnu-as -o task-1.o part-1.S
  aarch64-linux-gnu-ld -o task task-1.o
  ./task
  echo -e $"\nReturn code {$?}"
